626d50ffcdd1ee4ed01e2ab77dba5d346aa64b09
aryan4859 <aryan.shrestha@presidnetial.edu.np>
2025-01-12 11:44:49 -0500

    ..

--- app/(main)/authentication/page.tsx
+++ app/(main)/authentication/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
       <div className="absolute circlePosition w-screen sm:w-[590px] h-[400px] bg-gradient-to-r from-rose-500 rounded-[100%] top-[50%] left-[50%]  blur-[90px] translate-x-[-50%] translate-y-[-50%] z-[-1]" />
       <h1 className="text-3xl sm:text-3xl text-center  text-gray-600 leading-[51px]  sm:leading-1">
         Fly into{" "}
-        <span className="text-rose-700 font-extrabold">bingo CTF</span> Where Challenges Take Wings!🪽
+        <span className="text-rose-700 font-extrabold"> FlagForge</span> Where Challenges Take Wings!🪽
       </h1>
       <button
         onClick={() => signIn("google")}
