6881ed905416fa89e77ac12932502d6705c16095
aryan4859 <aryan.shrestha@presidnetial.edu.np>
2025-01-23 05:41:13 -0500

    Add security headers to next.config.js for enhanced app protection

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,36 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply headers to all routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://flagforge.aryan4.com.np", // Replace with your trusted domain
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' https://lh3.googleusercontent.com;",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
